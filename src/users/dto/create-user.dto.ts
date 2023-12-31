2c9cc7b87722976899e0f9b79be68c4a519d385f
Cerqueira <contatocerq@gmail.com>
2023-12-30 22:50:46 -0300

    Add custom messages to create-user-dto validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,17 +1,17 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O nome precisa ser uma string.' })
+  @IsNotEmpty({ message: 'O nome não pode ficar vazio.' })
   name: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O email precisa ser uma string.' })
+  @IsNotEmpty({ message: 'O email não pode ficar vazio.' })
   @IsEmail()
   email: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(8)
+  @IsString({ message: 'A senha precisa ser uma string.' })
+  @IsNotEmpty({ message: 'A senha não pode ficar vazia.' })
+  @MinLength(8, { message: 'A senha deve ter 8 ou mais caracteres.' })
   password: string;
 }
