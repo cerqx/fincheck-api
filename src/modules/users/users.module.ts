8e8bb344fd015239cbe3aab42fdf4412125c1687
Cerqueira <contatocerq@gmail.com>
2023-12-31 18:57:47 -0300

    GlobalModules created and updated

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
-import { PrismaService } from 'src/shared/database/prisma.service';
+import { DatabaseModule } from 'src/shared/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [UsersService, PrismaService, UsersRepository],
+  providers: [UsersService],
 })
 export class UsersModule {}
