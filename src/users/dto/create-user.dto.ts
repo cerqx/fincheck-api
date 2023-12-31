2a1cf612f24e25dfed2a5f1c25889e45e8ce1a1f
Cerqueira <contatocerq@gmail.com>
2023-12-31 11:30:34 -0300

    Sync changes

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,6 +3,7 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+  @IsEmail()
   name: string;
 
   @IsString()
