744756020889c6f9c481c9a1fc9fff250d59c65e
Cerqueira <contatocerq@gmail.com>
2024-01-01 19:31:51 -0300

    AuthenticateDto created

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthenticateDto } from './dto/authenticate.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
-  authenticate() {
+  authenticate(@Body() authenticateDto: AuthenticateDto) {
     return 'Authenticate';
   }
 }
