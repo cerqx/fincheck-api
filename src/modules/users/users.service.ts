406c4873a9e2bafc70246f34c6d5f4a1a42558eb
Cerqueira <contatocerq@gmail.com>
2024-01-03 10:09:23 -0300

    getUserById done

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -4,14 +4,13 @@ import { UsersRepository } from 'src/shared/database/repositories/users.reposito
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepo: UsersRepository) {}
-  async getUserById(userId: string) {
-    const user = await this.usersRepo.findUnique({
+  getUserById(userId: string) {
+    return this.usersRepo.findUnique({
       where: { id: userId },
+      select: {
+        name: true,
+        email: true,
+      },
     });
-
-    return {
-      name: user.name,
-      email: user.email,
-    };
   }
 }
