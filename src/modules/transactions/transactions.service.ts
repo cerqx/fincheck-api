97f2d6b81a5b8c4d3e6fc44247a7cea11613c232
Cerqueira <contatocerq@gmail.com>
2024-01-05 14:28:55 -0300

    create transactions created

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -2,12 +2,38 @@ import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repositories';
+import { ValidateBankAccountOwnershipService } from '../bank-accounts/services/validate-bank-account-ownership.service';
+import { ValidateCategoryOwnershipService } from '../categories/services/validate-category-ownership.service';
 
 @Injectable()
 export class TransactionsService {
-  constructor(private readonly transactionsRepo: TransactionsRepository) {}
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    private readonly transactionsRepo: TransactionsRepository,
+    private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
+    private readonly validateCategoryOwnershipService: ValidateCategoryOwnershipService,
+  ) {}
+
+  async create(userId: string, createTransactionDto: CreateTransactionDto) {
+    const { bankAccountId, categoryId, date, name, type, value } =
+      createTransactionDto;
+
+    await this.validateEntitiesOwnership({
+      userId,
+      bankAccountId,
+      categoryId,
+    });
+
+    return this.transactionsRepo.create({
+      data: {
+        userId,
+        bankAccountId,
+        categoryId,
+        name,
+        date,
+        type,
+        value,
+      },
+    });
   }
 
   findAllByUserId(userId: string) {
@@ -22,5 +48,20 @@ export class TransactionsService {
 
   remove(id: string) {
     return `This action removes a #${id} transaction`;
+  }
+
+  private async validateEntitiesOwnership({
+    bankAccountId,
+    categoryId,
+    userId,
+  }: {
+    userId: string;
+    bankAccountId: string;
+    categoryId: string;
+  }) {
+    await Promise.all([
+      this.validateBankAccountOwnershipService.validate(userId, bankAccountId),
+      this.validateCategoryOwnershipService.validate(userId, categoryId),
+    ]);
   }
 }
