cac09811179e5c83c32f532dbdf6b6e9e0d0da5d
Cerqueira <contatocerq@gmail.com>
2024-01-05 09:27:36 -0300

    Transactions findByUserId function created

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repositories';
 
 @Injectable()
 export class TransactionsService {
+  constructor(private readonly transactionsRepo: TransactionsRepository) {}
   create(createTransactionDto: CreateTransactionDto) {
     return 'This action adds a new transaction';
   }
 
   findAllByUserId(userId: string) {
-    return `This action returns all transactions`;
+    return this.transactionsRepo.findMany({
+      where: { userId },
+    });
   }
 
   update(id: string, updateTransactionDto: UpdateTransactionDto) {
