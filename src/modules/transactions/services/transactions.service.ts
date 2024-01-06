3b8b994d1ed8135c9ba638758c00f9ffbc3dd6f5
Cerqueira <contatocerq@gmail.com>
2024-01-05 23:36:26 -0300

    Transaction update

--- src/modules/transactions/services/transactions.service.ts
+++ src/modules/transactions/services/transactions.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { CreateTransactionDto } from '../dto/create-transaction.dto';
+import { UpdateTransactionDto } from '../dto/update-transaction.dto';
 import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repositories';
-import { ValidateBankAccountOwnershipService } from '../bank-accounts/services/validate-bank-account-ownership.service';
-import { ValidateCategoryOwnershipService } from '../categories/services/validate-category-ownership.service';
+import { ValidateBankAccountOwnershipService } from '../../bank-accounts/services/validate-bank-account-ownership.service';
+import { ValidateCategoryOwnershipService } from '../../categories/services/validate-category-ownership.service';
+import { ValidateTransactionOwnership } from './validate-transaction-ownership';
 
 @Injectable()
 export class TransactionsService {
@@ -11,6 +12,7 @@ export class TransactionsService {
     private readonly transactionsRepo: TransactionsRepository,
     private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
     private readonly validateCategoryOwnershipService: ValidateCategoryOwnershipService,
+    private readonly validateTransactionOwnershipService: ValidateTransactionOwnership,
   ) {}
 
   async create(userId: string, createTransactionDto: CreateTransactionDto) {
@@ -42,8 +44,32 @@ export class TransactionsService {
     });
   }
 
-  update(id: string, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
+  async update(
+    userId: string,
+    transactionId: string,
+    updateTransactionDto: UpdateTransactionDto,
+  ) {
+    const { name, date, type, value, bankAccountId, categoryId } =
+      updateTransactionDto;
+
+    await this.validateEntitiesOwnership({
+      userId,
+      bankAccountId,
+      categoryId,
+      transactionId,
+    });
+
+    return this.transactionsRepo.update({
+      where: { id: transactionId },
+      data: {
+        bankAccountId,
+        categoryId,
+        name,
+        date,
+        type,
+        value,
+      },
+    });
   }
 
   remove(id: string) {
@@ -54,12 +80,19 @@ export class TransactionsService {
     bankAccountId,
     categoryId,
     userId,
+    transactionId,
   }: {
     userId: string;
     bankAccountId: string;
     categoryId: string;
+    transactionId?: string;
   }) {
     await Promise.all([
+      transactionId &&
+        this.validateTransactionOwnershipService.validate(
+          userId,
+          transactionId,
+        ),
       this.validateBankAccountOwnershipService.validate(userId, bankAccountId),
       this.validateCategoryOwnershipService.validate(userId, categoryId),
     ]);
--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TransactionsService } from './transactions.service';
+import { TransactionsService } from './services/transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { BankAccountsModule } from '../bank-accounts/bank-accounts.module';
 import { CategoriesModule } from '../categories/categories.module';
+import { ValidateTransactionOwnership } from './services/validate-transaction-ownership';
 
 @Module({
   imports: [BankAccountsModule, CategoriesModule],
   controllers: [TransactionsController],
-  providers: [TransactionsService],
+  providers: [TransactionsService, ValidateTransactionOwnership],
 })
 export class TransactionsModule {}
