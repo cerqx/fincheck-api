79e49f570b4f47f733abfc09d176cacdd72f92be
Cerqueira <contatocerq@gmail.com>
2024-01-08 22:34:28 -0300

    app.enableCors ready

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
